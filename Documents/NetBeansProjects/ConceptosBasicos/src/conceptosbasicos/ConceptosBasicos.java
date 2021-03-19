9e63e500c563145ea783c5cab36b618b549bbf0d
pepinadas <mmmunapregunta@gmail.com>
2021-03-19 17:17:39 -0600

    Aprendiendo 19_03_2021

---
+++ Documents/NetBeansProjects/ConceptosBasicos/src/conceptosbasicos/ConceptosBasicos.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conceptosbasicos;
+
+import ico.fes.Automovil;
+import java.awt.Color;
+
+/**
+ *
+ * @author Personal
+ */
+public class ConceptosBasicos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Automovil miBocho = new Automovil();
+        //miBocho.marca="volkswagen";
+        miBocho.setMarca("VW");
+        miBocho.setSubMarca("Sedan");
+        miBocho.setModelo(1870);
+        miBocho.setColor(Color.blue);
+        System.out.println(miBocho);
+        
+        //Tarea.- Programar los objetos miAcura y miMustang
+    }
+    
+}
